cd45ca864b1725de8e476bc3b7f4529fc5df6194
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-09-24 17:25:50 -0600

    addes resources, soccer working

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Reader = require('./src/WeatherReader');
-
+const Reader2 = require('./src/SoccerReader');
 // const FILENAME = 'weather.dat';
 
 
 // weather.readFile(FILENAME);
 
 console.log(Reader.desiredDay());
+console.log(Reader2.closestTeam());
