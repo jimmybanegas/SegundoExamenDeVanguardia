505d2c4805924bf2dd9752cb87130f0cdeb1323a
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-09-25 14:08:51 -0600

    spec files for all classes

--- spec/soccer-reader-spec.js
+++ spec/soccer-reader-spec.js
@@ -1,6 +1,32 @@
-// const soccer = require('../src/soccer-reader');
-// require('jasmine-co').install();
+const soccer = require('../src/soccer-reader');
+require('jasmine-co').install();
 
-// describe('SoccerReader class ', () => {
+describe('SoccerReader class ', () => {
+  describe('when parsed data is correct', () => {
+    let data;
+    beforeEach(() => {
+      data = soccer.parseData();
+    });
+    it('matches objects with the expect key/value pairs', () => {
+      expect(data[0]).toEqual(jasmine.objectContaining({
+        team: 'Arsenal',
+        goalsFor: 79,
+        goalsAgainst: 36,
+      }));
+    });
+  });
 
-// });
+  describe('finds the closest', () => {
+    let data;
+    beforeEach(() => {
+      data = soccer.getClosestTeam;
+    });
+    it('matches objects with the expect key/value pairs', () => {
+      expect(data[0]).toEqual(jasmine.objectContaining({
+        team: 'Aston_Villa',
+        goalsFor: 46,
+        goalsAgainst: 47,
+      }));
+    });
+  });
+});
--- spec/weather-reader-spec.js
+++ spec/weather-reader-spec.js
@@ -1,7 +1,33 @@
-// const weather = require('../src/weather-reader');
-// require('jasmine-co').install();
+const weather = require('../src/weather-reader');
+require('jasmine-co').install();
 
-// describe('WeatherReader class ', () => {
+describe('WeatherReader class ', () => {
+  describe('when parsed data is correct', () => {
+    let data;
+    beforeEach(() => {
+      data = weather.parseData();
+    });
+    it('matches objects with the expect key/value pairs', () => {
+      expect(data[0]).toEqual(jasmine.objectContaining({
+        day: 1,
+        maximum: 88,
+        minimum: 59,
+      }));
+    });
+  });
 
-// });
+  describe('finds the day with minimum spread', () => {
+    let data;
+    beforeEach(() => {
+      data = weather.getDesiredDay();
+    });
+    it('matches objects with the expect key/value pairs', () => {
+      expect(data[0]).toEqual(jasmine.objectContaining({
+        day: 14,
+        maximum: 61,
+        minimum: 59,
+      }));
+    });
+  });
+});
 
