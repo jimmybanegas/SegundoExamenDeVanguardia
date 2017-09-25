ba0c45c29d29a0087727dbed89cc88e0c57407c1
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-09-24 22:56:55 -0600

    rename variables

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const Reader = require('./src/WeatherReader');
-const Reader2 = require('./src/SoccerReader');
+const Reader = require('./src/weather-reader');
+const Reader2 = require('./src/soccer-reader');
 // const FILENAME = 'weather.dat';
 
 
 // weather.readFile(FILENAME);
 
-console.log(Reader.desiredDay());
-console.log(Reader2.closestTeam());
+console.log(Reader.getDesiredDay());
+console.log(Reader2.getClosestTeam());
---
+++ src/reader.js
@@ -0,0 +1,8 @@
+const FS = require('fs');
+
+module.exports = class Reader {
+  readFile(filename) {
+    return FS.readFileSync(filename, 'utf8').trim();
+  }
+};
+
